5d1211ed0cd1814f2e1a398e55be80b9f739ff43
elino <jankovskajae@yahoo.com>
2024-10-31 16:22:26 -0500

    advancement

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-sleep 10
-FICHIER=exists.txt
+FICHIER="exists.txt"
 
-if [ -f "$FICHIER" ]; then
+mkdir -p /run/php/ /var/www/html
+
+if [ -f "/var/www/html/$FICHIER" ]; then
     echo "Le fichier de configuration existe ($FICHIER)"
 else
     echo "Le fichier de configuration n'existe pas ($FICHIER). On demarre l'installation"
 
-    ./wp-cli.phar core download
+	cd /var/www/html
+    wp-cli.phar core download
 
-	./wp-cli.phar config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 #--dbprefix=wp_ # --path='/var/www/wordpress'
+	wp-cli.phar config create \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" \
+        --dbhost="mariadb:3306"
+
 	# to avoid discolsing admin password to bash history (the usefulness is to be talked about)
 	# wp core install --url=yourdomain.com --title=Site_Title --admin_user=admin_username --admin_email=your@email.com --prompt=admin_password < admin_password.txt
 	# or
-	./wp-cli.phar core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com
-	./wp-cli.phar user create bob bob@example.com --role=author
-	if [ -d "/run/php/" ]; then
-		echo "/run/php already exists"
-	else
-		echo "creating /run/php"
-		mkdir -p /run/php/
-	fi
-	touch $FICHIER
+	wp-cli.phar core install --url=$DOMAIN_NAME --title=Example --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
+	wp-cli.phar user create bob bob@example.com --role=author
+
+	touch /var/www/html/$FICHIER
 fi
 php-fpm82 -F -R
