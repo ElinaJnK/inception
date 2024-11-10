ff63fb0b4d289212e9788448363d3cbd52077226
elino <jankovskajae@yahoo.com>
2024-11-10 13:39:06 -0600

    redis cache

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -20,10 +20,18 @@ else
         --dbuser="$MYSQL_USER" \
         --dbpass="$mysql_pass" \
         --dbhost="mariadb:3306"
+		--extra-php <<PHP
+define( 'WP_REDIS_HOST', 'redis' );
+define( 'WP_REDIS_PORT', '6379' );
+define('WP_REDIS_PATH', __DIR__ . '/../redis.sock');
+define('WP_REDIS_SCHEME', 'unix');
+PHP
 
 	wp-cli.phar core install --url=$DOMAIN_NAME --title=Site_Title --admin_user=$ADMIN_USER --admin_email=your@email.com --allow-root --prompt=admin_password < ${WORDPRESS_ADMIN_PASS} > /dev/null 2>&1
 	wp-cli.phar user create $LOGIN $LOGIN@example.com --role=author --allow-root --prompt=user_pass < ${WORDPRESS_USER_PASS} > /dev/null 2>&1
 
+    wp-cli.phar plugin install redis-cache --activate
+    
 	touch /var/www/html/$FICHIER
 fi
 php-fpm82 -F -R
